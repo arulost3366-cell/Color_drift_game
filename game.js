72e97695b4b2b95ac0c346e67c4b0e25bd3bddcc
arulost3366-cell <arulost3366@gmail.com>
2026-03-13 13:13:12 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,80 +1,137 @@
-let score = 0
+const playArea = document.getElementById("playArea")
 
 const colors = ["blue","yellow","red"]
 
-const ballsContainer = document.getElementById("balls")
-const bins = document.querySelectorAll(".bin")
+let score = 0
+let time = 300
 
-function createBall(){
+function random(min,max){
+return Math.random()*(max-min)+min
+}
 
-let color = colors[Math.floor(Math.random()*colors.length)]
+class Ball{
 
-let ball = document.createElement("div")
-ball.className = "ball " + color
-ball.draggable = true
-ball.dataset.color = color
+constructor(){
 
-ball.addEventListener("dragstart", dragStart)
+this.color = colors[Math.floor(Math.random()*colors.length)]
 
-ballsContainer.appendChild(ball)
+this.el = document.createElement("div")
+this.el.className = "ball " + this.color
+this.el.draggable = true
+
+this.x = random(50,800)
+this.y = random(50,400)
+
+this.vx = random(-0.3,0.3)
+this.vy = random(-0.3,0.3)
+
+this.el.style.left = this.x+"px"
+this.el.style.top = this.y+"px"
+
+this.el.dataset.color = this.color
+
+playArea.appendChild(this.el)
+
+this.el.addEventListener("dragstart",e=>{
+e.dataTransfer.setData("color",this.color)
+e.dataTransfer.setData("id",this.el)
+})
 
 }
+
+move(){
+
+this.x+=this.vx
+this.y+=this.vy
+
+if(this.x<0||this.x>850)this.vx*=-1
+if(this.y<0||this.y>450)this.vy*=-1
+
+this.el.style.left=this.x+"px"
+this.el.style.top=this.y+"px"
+
+}
+
+}
+
+let balls=[]
 
 function spawnBalls(){
 
-for(let i=0;i<8;i++){
-createBall()
-}
-
-}
-
-function dragStart(e){
-
-e.dataTransfer.setData("color", e.target.dataset.color)
-e.dataTransfer.setData("id", e.target)
-
-}
-
-bins.forEach(bin=>{
-
-bin.addEventListener("dragover", e=>{
-e.preventDefault()
-})
-
-bin.addEventListener("drop", dropBall)
-
-})
-
-function dropBall(e){
-
-e.preventDefault()
-
-let color = e.dataTransfer.getData("color")
-
-let dragged = document.querySelector(".ball."+color)
-
-if(!dragged) return
-
-if(color === e.currentTarget.dataset.color){
-
-score++
-
-document.getElementById("score").innerText = score
-
-dragged.remove()
-
-createBall()
-
-}else{
-
-dragged.style.transform = "scale(0.8)"
-
-setTimeout(()=>{
-dragged.style.transform="scale(1)"
-},200)
-
+for(let i=0;i<10;i++){
+balls.push(new Ball())
 }
 
 }
 
 spawnBalls()
+
+function animate(){
+
+balls.forEach(b=>{
+b.move()
+})
+
+requestAnimationFrame(animate)
+
+}
+
+animate()
+
+const bins = document.querySelectorAll(".bin")
+
+bins.forEach(bin=>{
+
+bin.addEventListener("dragover",e=>e.preventDefault())
+
+bin.addEventListener("drop",e=>{
+
+e.preventDefault()
+
+let color = e.dataTransfer.getData("color")
+
+if(color === bin.dataset.color){
+
+score++
+
+document.getElementById("score").innerText = score
+
+spawnBalls()
+
+}else{
+
+score--
+
+document.getElementById("score").innerText = score
+
+bin.style.transform="scale(0.9)"
+
+setTimeout(()=>{
+bin.style.transform="scale(1)"
+},200)
+
+}
+
+})
+
+})
+
+function updateTimer(){
+
+time--
+
+let m=Math.floor(time/60)
+let s=time%60
+
+document.getElementById("time").innerText =
+m+":"+(s<10?"0":"")+s
+
+if(time<=0){
+
+alert("Time finished")
+
+}
+
+}
+
+setInterval(updateTimer,1000)
